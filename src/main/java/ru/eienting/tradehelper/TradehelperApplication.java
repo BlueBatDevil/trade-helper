02c22aab74fa624c79763834d68c0bd57331241e
BlueBatDevil <bluebatriaw@gmail.com>
2023-08-22 10:41:43 +0000

    project has been initialized

---
+++ src/main/java/ru/eienting/tradehelper/TradehelperApplication.java
@@ -0,0 +1,13 @@
+package ru.eienting.tradehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradehelperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TradehelperApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/eienting/tradehelper/TradehelperApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.eienting.tradehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradehelperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
